4b148b926789894421e5a56889cb5ad1551751cb
mprosser994 <mprosser@nd.edu>
2017-10-12 15:07:57 -0400

    Fix bug causing app to crash, add asking for permissions

--- app/src/main/java/com/example/mpros/myfirstapp/DisplaySysActivity.java
+++ app/src/main/java/com/example/mpros/myfirstapp/DisplaySysActivity.java
@@ -2,11 +2,14 @@ package com.example.mpros.myfirstapp;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,34 +17,15 @@ import android.widget.TextView;
 
 public class DisplaySysActivity extends AppCompatActivity {
 
-    private TextView textView = (TextView) findViewById(R.id.locationText);
-    private LocationManager manager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-    private LocationListener listener = new LocationListener() {
-        @Override
-        public void onLocationChanged(Location location) {
-            setText(textView, location);
-        }
-
-        @Override
-        public void onStatusChanged(String s, int i, Bundle bundle) {
-
-        }
-
-        @Override
-        public void onProviderEnabled(String s) {
-            // Do I need this?
-        }
-
-        @Override
-        public void onProviderDisabled(String s) {
-            // Raise alert message
-        }
-    };
+    private LocationManager manager;
+    private LocationListener listener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_sys);
+        setupManager();
+        askForPermissions();
     }
 
     public void updateLocation(View view) {
@@ -51,6 +35,7 @@ public class DisplaySysActivity extends AppCompatActivity {
                 PackageManager.PERMISSION_GRANTED) {
             location = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
             if (location != null) {
+                TextView textView = (TextView) findViewById(R.id.locationText);
                 setText(textView, location);
             }
             else {
@@ -60,11 +45,24 @@ public class DisplaySysActivity extends AppCompatActivity {
                 PackageManager.PERMISSION_GRANTED) {
             location = manager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
             if (location != null) {
+                TextView textView = (TextView) findViewById(R.id.locationText);
                 setText(textView, location);
             }
             else {
                 manager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, listener);
             }
+        } else {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Permissions Needed")
+                    .setMessage("Please allow this app to access your location.")
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            askForPermissions();
+                        }
+                    })
+                    .show();
+
         }
     }
 
@@ -72,5 +70,38 @@ public class DisplaySysActivity extends AppCompatActivity {
         double lat = location.getLatitude();
         double lon = location.getLongitude();
         textView.setText("Lat.: " + lat + ", Long.: " + lon);
+    }
+
+    private void setupManager() {
+        manager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                TextView textView = (TextView) findViewById(R.id.locationText);
+                setText(textView, location);
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+                // Do I need this?
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+                // Raise alert message
+            }
+        };
+    }
+
+    private void askForPermissions() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION},
+                0);
     }
 }
