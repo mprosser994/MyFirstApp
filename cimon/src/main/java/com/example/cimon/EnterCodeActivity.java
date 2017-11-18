e8445899e8e64e08c4cd8b4c3e63c455e84c92b2
mprosser994 <mprosser@nd.edu>
2017-11-17 21:00:26 -0500

    Web API Integration
    Added code to actually invoke API, tightened up use of SharedPreferences

--- cimon/src/main/java/com/example/cimon/EnterCodeActivity.java
+++ cimon/src/main/java/com/example/cimon/EnterCodeActivity.java
@@ -1,40 +1,116 @@
 package com.example.cimon;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.system.ErrnoException;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class EnterCodeActivity extends AppCompatActivity {
+
+    public CimonService service;
+    private SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_code);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://129.74.247.110/cimoninterface/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        service = retrofit.create(CimonService.class);
+
+        prefs = this.getSharedPreferences(
+                getString(R.string.prefs_file_key), Context.MODE_PRIVATE
+        );
+        String email = prefs.getString("email", "");
+        String msg = getString(R.string.notif, email);
+        TextView textView = (TextView) findViewById(R.id.txt_notif);
+        textView.setText(msg);
     }
 
     public void ConfirmCode(View view) {
         EditText code_txt = (EditText) findViewById(R.id.txt_code);
         String code = code_txt.getText().toString();
-        // Invoke web service to validate code
-        // If web service returns true:
-        if (code.equals("1234")) { // placeholder
-            // Set user state to "Logged In"
-            SharedPreferences prefs = this.getSharedPreferences(
-                    getString(R.string.prefs_file_key), Context.MODE_PRIVATE
-            );
-            SharedPreferences.Editor editor = prefs.edit();
-            editor.putString(getString(R.string.login_state), getResources().getString(R.string.login_state_logged_in));
-            editor.apply();
 
-            // Start main activity
-            startActivity(new Intent(this, MainActivity.class));
-        } else {
-            code_txt.setError("The code you entered is incorrect");
+        // Get UUID to send to service
+        String uuid = prefs.getString("uuid", "");
+        String email = prefs.getString("email", "");
+
+        Call<CimonResponse> call = service.verifyToken(email, uuid, code);
+        try {
+            call.enqueue(new Callback<CimonResponse>() {
+                @Override
+                public void onResponse(Call<CimonResponse> call, Response<CimonResponse> response) {
+                    Log.d("Response from server", String.valueOf(response.body().getCode()) + ", " + response.body().getMessage());
+                    if (response.body().getCode() == 0) {
+                        startNextActivity();
+                    } else {
+                        handleServerError(response.body().getCode());
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<CimonResponse> call, Throwable t) {
+                    showError((Exception) t);
+                }
+            });
+        }catch (Exception e){
+            showError(e);
         }
+    }
+
+    private void showError(Exception e) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Error")
+                .setMessage("Exception: " + e.getMessage())
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {}
+                })
+                .show();
+    }
+
+    private void handleServerError(int code) {
+        switch (code) {
+            case -1:
+                showError(new Exception("Invalid token"));
+                break;
+            case -2:
+                showError(new Exception("Invalid input"));
+                break;
+            case -3:
+                showError(new Exception("Token mismatch"));
+                break;
+            case -9:
+            default:
+                showError(new Exception("Server error"));
+                break;
+        }
+    }
+
+    private void startNextActivity() {
+        // Set user state to "Logged In"
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(getString(R.string.login_state), getResources().getString(R.string.login_state_logged_in));
+        editor.apply();
+
+        startActivity(new Intent(this, MainActivity.class));
     }
 
     @Override
--- cimon/src/main/java/com/example/cimon/GetEmailActivity.java
+++ cimon/src/main/java/com/example/cimon/GetEmailActivity.java
@@ -1,20 +1,46 @@
 package com.example.cimon;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
 
+import java.util.UUID;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class GetEmailActivity extends AppCompatActivity {
+
+    public CimonService service;
+    private SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_email);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://129.74.247.110/cimoninterface/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        service = retrofit.create(CimonService.class);
+
+        prefs = this.getSharedPreferences(
+                getString(R.string.prefs_file_key), Context.MODE_PRIVATE
+        );
+
+        generateUUID();
     }
 
     public void SendCode(View view) {
@@ -22,19 +48,80 @@ public class GetEmailActivity extends AppCompatActivity {
         String email = email_txt.getText().toString();
 
         if (Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            // Call web service to store email in DB, have web service send email
-            // Set user state to "Code Sent"
-            SharedPreferences prefs = this.getSharedPreferences(
-                    getString(R.string.prefs_file_key), Context.MODE_PRIVATE
-            );
+            // Get UUID to send to service
+            String uuid = prefs.getString("uuid", "");
+
+            // Save email
             SharedPreferences.Editor editor = prefs.edit();
-            editor.putString(getString(R.string.login_state), getResources().getString(R.string.login_state_code_sent));
+            editor.putString("email", email);
             editor.apply();
 
-            startActivity(new Intent(this, EnterCodeActivity.class));
+            Call<CimonResponse> call = service.signup(email, uuid);
+            try {
+                call.enqueue(new Callback<CimonResponse>() {
+                    @Override
+                    public void onResponse(Call<CimonResponse> call, Response<CimonResponse> response) {
+                        Log.d("Response from server", String.valueOf(response.body().getCode()) + ", " + response.body().getMessage());
+                        if (response.body().getCode() == 0) {
+                            startNextActivity();
+                        } else {
+                            handleServerError(response.body().getCode());
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<CimonResponse> call, Throwable t) {
+                        showError((Exception) t);
+                    }
+                });
+            }catch (Exception e){
+                showError(e);
+            }
         } else {
             email_txt.setError("Please enter a valid email address");
         }
+    }
+
+    private void generateUUID() {
+        String uuid = prefs.getString("uuid", "");
+        if (uuid.equals("")) {
+            UUID u = UUID.randomUUID();
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putString("uuid", u.toString());
+            editor.apply();
+        }
+    }
+
+    private void showError(Exception e) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Error")
+                .setMessage("Exception: " + e.getMessage())
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {}
+                })
+                .show();
+    }
+
+    private void handleServerError(int code) {
+        switch (code) {
+            case -1:
+                showError(new Exception("Invalid input"));
+                break;
+            case -9:
+            default:
+                showError(new Exception("Server error"));
+                break;
+        }
+    }
+
+    private void startNextActivity() {
+        // Set user state to "Code Sent"
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(getString(R.string.login_state), getResources().getString(R.string.login_state_code_sent));
+        editor.apply();
+
+        startActivity(new Intent(this, EnterCodeActivity.class));
     }
 
     @Override
