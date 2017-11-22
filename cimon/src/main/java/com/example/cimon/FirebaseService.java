b987d944aafacc28c949b68c8bbdc19ab0adbfab
mprosser994 <mprosser@nd.edu>
2017-11-22 14:36:32 -0500

    Clean up imports, remove currently unneeded code

--- cimon/src/main/java/com/example/cimon/FirebaseService.java
+++ cimon/src/main/java/com/example/cimon/FirebaseService.java
@@ -1,7 +1,6 @@
 package com.example.cimon;
 
 import android.util.Log;
-
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
@@ -17,10 +16,11 @@ public class FirebaseService extends FirebaseInstanceIdService {
         // Get updated InstanceID token.
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
         Log.d(TAG, "Refreshed token: " + refreshedToken);
+        // save this locally, use for messaging later
 
         // If you want to send messages to this application instance or
         // manage this apps subscriptions on the server side, send the
         // Instance ID token to your app server.
-        sendRegistrationToServer(refreshedToken);
+        //sendRegistrationToServer(refreshedToken);
     }
 }
